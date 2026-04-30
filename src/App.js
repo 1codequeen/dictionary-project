af832a55ae20bb06582712b43f196d518b28d90e
1codequeen <queenyona21@gmail.com>
2026-04-30 16:38:16 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center mt-5">Welcome to the Dictionary App</h1>
+    </div>
+  );
+}
+
+
